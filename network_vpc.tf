96b82aa4c2b0664777821017eadf7d45e8a90740
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-04-13 19:46:08 -0300

    refactor: add environment variable to VPC module

--- network_vpc.tf
+++ network_vpc.tf
@@ -2,6 +2,8 @@ module "vpc" {
   source = "../container-arch--aws-ecs-module/network"
 
   project_name = local.workspace.project_name
+  environment  = local.workspace.environment
+
   cidr_block   = local.workspace.cidr_block
   subnet_count = local.workspace.subnet_count
 }
