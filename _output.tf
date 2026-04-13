aa45f1b7b518c43e551c526f9711f986b88da04e
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-04-13 17:32:41 -0300

    feat: stop using ssm parameter for values that can be taken with data

--- _output.tf
+++ _output.tf
@@ -1,15 +1,19 @@
-output "ssm_vpc_id" {
-  value = aws_ssm_parameter.vpc_id.id
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
-output "ssm_private_subnet_ids" {
-  value = { for az in local.aws_vpc_azs : az => aws_ssm_parameter.private_subnet_ids[az].id }
+output "vpc_cidr" {
+  value = aws_vpc.main.cidr_block
 }
 
-output "ssm_public_subnet_ids" {
-  value = { for az in local.aws_vpc_azs : az => aws_ssm_parameter.public_subnet_ids[az].id }
+output "vpc_private_subnet_ids" {
+  value = { for az in local.aws_vpc_azs : az => aws_subnet.these_private[az].id }
 }
 
-output "ssm_data_subnet_ids" {
-  value = { for az in local.aws_vpc_azs : az => aws_ssm_parameter.data_subnet_ids[az].id }
+output "vpc_public_subnet_ids" {
+  value = { for az in local.aws_vpc_azs : az => aws_subnet.these_public[az].id }
+}
+
+output "vpc_data_subnet_ids" {
+  value = { for az in local.aws_vpc_azs : az => aws_subnet.these_data[az].id }
 }
