e82ef6bbfc07bd958481a90530506e8e6940a6a8
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-04-13 18:22:18 -0300

    refactor: rename output variables for clarity

--- _output.tf
+++ _output.tf
@@ -1,19 +1,19 @@
-output "vpc_id" {
+output "id" {
   value = aws_vpc.main.id
 }
 
-output "vpc_cidr" {
+output "cidr_block" {
   value = aws_vpc.main.cidr_block
 }
 
-output "vpc_private_subnet_ids" {
+output "private_subnet_ids" {
   value = { for az in local.aws_vpc_azs : az => aws_subnet.these_private[az].id }
 }
 
-output "vpc_public_subnet_ids" {
+output "public_subnet_ids" {
   value = { for az in local.aws_vpc_azs : az => aws_subnet.these_public[az].id }
 }
 
-output "vpc_data_subnet_ids" {
+output "data_subnet_ids" {
   value = { for az in local.aws_vpc_azs : az => aws_subnet.these_data[az].id }
 }
