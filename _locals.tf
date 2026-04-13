2f1b175810db890054aa6fddfd4ce654c0c16842
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-04-13 19:52:23 -0300

    refactor: simplify tags in locals.tf to include only owner

--- _locals.tf
+++ _locals.tf
@@ -2,10 +2,6 @@ locals {
   workspace = var.workspaces[terraform.workspace]
 
   tags = {
-    Project     = local.workspace.project_name
-    Region      = local.workspace.aws_region
-    Environment = local.workspace.environment
-    Workspace   = terraform.workspace
-    ManagedBy   = "Terraform"
+    Owner = "DevOps Team"
   }
 }
