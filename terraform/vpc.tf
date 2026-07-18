4e99a6789c8c271856eb443b9f688ece86db396c
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-07-18 03:09:46 -0300

    extra: update source

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/therenanlira/container-arch--aws-ecs-module.git//network?ref=v1"
+  source = "git::https://github.com/therenanlira/container-arch--aws-modules.git//network?ref=v1"
 
   project_name = local.workspace.project_name
   environment  = local.workspace.environment
