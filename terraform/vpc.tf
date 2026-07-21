2ed4e1ab919f50a66764e2990e7557c2a7d95972
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-07-21 00:59:42 -0300

    aula8: restarting (#3)

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/therenanlira/container-arch--aws-modules.git//network?ref=v1"
+  source = "git::https://github.com/therenanlira/container-arch--aws-modules.git//vpc_network?ref=v1"
 
   project_name = local.workspace.project_name
   environment  = local.workspace.environment
