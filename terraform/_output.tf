9430bce2ab2507b29cc22d5bbf2cbf0cd59b60e6
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-08-05 18:43:06 -0300

    aula9: restarting

--- terraform/_output.tf
+++ terraform/_output.tf
@@ -17,3 +17,11 @@ output "public_subnet_ids" {
 output "data_subnet_ids" {
   value = module.vpc.data_subnet_ids
 }
+
+output "dns_zone_id" {
+  value = module.vpc.dns_zone_id
+}
+
+output "dns_name" {
+  value = module.vpc.dns_name
+}
