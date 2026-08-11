6f7ad642fdc65752bbc148904bd89775490d0f92
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-08-11 17:32:23 -0300

    aula14 (#7)

--- terraform/account_settings.tf
+++ terraform/account_settings.tf
@@ -1,6 +1,6 @@
 module "account_settings" {
-  count  = local.workspace.is_central ? 1 : 0
   source = "git::https://github.com/therenanlira/container-arch--aws-modules.git//account_config?ref=v1"
 
+  environment         = local.workspace.environment
   api_gateway_logging = true
 }
